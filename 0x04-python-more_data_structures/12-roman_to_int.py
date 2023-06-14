17669b4a7f808322f7d8434648f997c22482d1ea
Jerry-kjk <jerrykajik@gmail.com>
2023-06-14 13:47:42 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
-roman_num = {'I': 1, 'V': 5, 'IX': 9, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 def roman_to_int(roman_string):
-    x = 0
-    if not isinstance(roman_string, str):
-        return x
-    r_numeral = roman_num.keys()
-    n = len(roman_string)
-    i = 0
-    while i < n:
-        result_1 = roman_string[i:i+2]
-        result_2 = roman_string[i]
-        if result_1 in r_numeral:
-            x += roman_num.get(result_1)
-            i += 2
-        elif result_2 in r_numeral:
-            x += roman_num.get(result_2)
+    if isinstance(roman_string, str):
+        val = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+        spec = dict(I='VX', X='LC', C='DM')
+        total = 0
+        i = 0
+        s_len = len(roman_string)
+        while i < s_len:
+            c = roman_string[i]
+            if c in spec.keys():
+                if i + 1 < s_len and roman_string[i + 1] in spec[c]:
+                    total += val[roman_string[i + 1]] - val[c]
+                    i += 1
+                else:
+                    total += val[c]
+            else:
+                total += val[c]
             i += 1
-
-    return x
+        return total
+    else:
+        return 0
