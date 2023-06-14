25a9b119d65f8af0f9d34a54bd59cc47759e7379
Jerry-kjk <jerrykajik@gmail.com>
2023-06-14 04:05:59 -0700

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def best_score(a_dictionary):
+    if a_dictionary:
+        max_score = max(a_dictionary.values())
+        for i, j in a_dictionary.items():
+            if j == max_score:
+                return (i)
+        return (None)
