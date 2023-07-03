61ea29f5525c8f5ce61ced014003b866e5026837
Jerry-kjk <jerrykajik@gmail.com>
2023-07-03 14:07:49 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
+"""Solves the N-queens puzzle.
 
-"""
-The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard.
-"""
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
 
+Example:
+    $ ./101-nqueens.py N
+
+N must be an integer greater than or equal to 4.
+
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
 import sys
 
 
-def is_safe(board, row, col):
-    """
-    Check if it's safe to place a queen at board[row][col]
-    """
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
