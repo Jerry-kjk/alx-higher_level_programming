663fb64df1d8cf1340b2db8ffa318c8c517be259
Jerry-kjk <jerrykajik@gmail.com>
2023-07-11 09:10:25 -0700

    Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -15,18 +15,13 @@ def __init__(self, first_name, last_name, age):
         """
         If attrs is not provided, retrieve all attributes.
         """
-        if isinstance(attrs, list):
-            if all(isinstance(attr, str) for attr in attrs):
-                res = {}
-                for i in attrs:
-                    if i in self.__dict__:
-                        res[i] = self.__dict__[i]
-                return res
-        return self.__dict__
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
     def reload_from_json(self, json):
         """
         Replace all attributes of the Student instance
         with the key-value pairs from the JSON dictionary
         """
-        for attr in json:
-            self.__dict__[attr] = json[attr]
+        for key, value in json.items():
+            setattr(self, key, value)
