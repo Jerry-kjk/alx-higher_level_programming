b2e6fac710fb50dadc5eb2fb25477b6f888a9197
Jerry-kjk <jerrykajik@gmail.com>
2023-07-12 07:16:47 -0700

    Update file

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,13 +4,19 @@ Defines a class Student.
 """
 
 
-def __init__(self, first_name, last_name, age):
+class Student:
+    """
+    Student class.
+    """
+
+    def __init__(self, first_name, last_name, age):
         """
         Initialize the public instance attributes.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+    
     def to_json(self, attrs=None):
         """
         If attrs is not provided, retrieve all attributes.
@@ -18,6 +24,7 @@ def __init__(self, first_name, last_name, age):
         if attrs is None or not isinstance(attrs, list):
             return self.__dict__
         return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+    
     def reload_from_json(self, json):
         """
         Replace all attributes of the Student instance
