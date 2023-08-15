3bc627b4c7639a98203a4d6da61b22f29aae3cd2
Jerry-kjk <jerrykajik@gmail.com>
2023-08-15 05:38:40 -0700

    First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Used to insert a new row into the table first_table in the specified database.
+-- Insert a new row into the table first_table.
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
