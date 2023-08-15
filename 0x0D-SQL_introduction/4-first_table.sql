456b4afb8ddecf87b0ad0d91edac2e6e4e80e186
Jerry-kjk <jerrykajik@gmail.com>
2023-08-15 05:22:19 -0700

    List tables

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- used to create the table first_table in the current database.
+-- Create the table first_table if it doesn't exist.
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+	name VARCHAR(256)
+	);
