97ada13c84e787f864d4876de867865572ba516f
Jerry-kjk <jerrykajik@gmail.com>
2023-08-15 07:37:00 -0700

    List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Used to list all records of the table second_table from the specified database.
 -- List all records of the table second_table, ordered by score (top first).
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
