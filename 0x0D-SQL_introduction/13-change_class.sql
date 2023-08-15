770d5c88e00518abe203c8467fe7a2fef43c3fe8
Jerry-kjk <jerrykajik@gmail.com>
2023-08-15 07:46:05 -0700

    Score too low

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Used to remove all records with a score <= 5 from the table second_table in the specified database.
+-- Remove all records with a score <= 5 from the table second_table.
+DELETE FROM second_table WHERE score <= 5;
