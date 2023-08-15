3e2530e84c5a6be4ca19963600dda24c35bb331e
Jerry-kjk <jerrykajik@gmail.com>
2023-08-15 07:50:56 -0700

    Average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Used to compute the score average of all records in the table second_table in the specified database.
+-- Compute the average score of all records in the table second_table.
+SELECT AVG(score) AS average FROM second_table;
