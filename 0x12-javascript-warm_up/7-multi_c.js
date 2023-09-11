3480af1e72a6c56b248b35a136da8fd3651b6ed2
Jerry-kjk <jerrykajik@gmail.com>
2023-09-11 07:42:41 -0700

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+const x = parseInt(arg[2]);
+let loop = 0;
+
+if (isNaN(x)) {
+	console.log('Missing number of occurrences');
+} else {
+	while (loop < x) {
+		console.log('C is fun');
+		loop++;
+	}
+}
