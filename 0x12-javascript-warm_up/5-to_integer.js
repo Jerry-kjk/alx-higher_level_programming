8fbc2d3b64b492429d49a350927c3d901356a5a3
Jerry-kjk <jerrykajik@gmail.com>
2023-09-11 05:49:59 -0700

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
+const entry = process.argv;
 
-const num = parseInt(arg);
-if (!isNaN(num)) {
-	console.log("My number: " + num);
-}  else {
-	console.log("Not a number");
+const toNumber = parseInt(entry[2]);
+if (isNaN(toNumber)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', toNumber);
 }
