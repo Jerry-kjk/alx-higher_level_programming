4ad6ac3a4a166bf7f6293ee999e362a30273200f
Jerry-kjk <jerrykajik@gmail.com>
2023-09-12 07:26:19 -0700

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,10 +5,10 @@ const x = parseInt(arg[2]);
 let loop = 0;
 
 if (isNaN(x)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	while (loop < x) {
-		console.log('C is fun');
-		loop++;
-	}
+  while (loop < x) {
+    console.log('C is fun');
+    loop++;
+  }
 }
