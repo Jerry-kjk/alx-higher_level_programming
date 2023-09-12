511c2c41e47fb1cad690bda7ba4e115332e96305
Jerry-kjk <jerrykajik@gmail.com>
2023-09-12 07:56:13 -0700

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,9 +5,9 @@ const side = parseInt(entry[2]);
 let x;
 
 if (isNaN(side)) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	for (x = 0; x < side; x++) {
-		console.log('X'.repeat(side));
-	}
+  for (x = 0; x < side; x++) {
+    console.log('X'.repeat(side));
+  }
 }
