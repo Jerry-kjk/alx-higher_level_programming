6285aa691f21a8b00f63d2482c3cf4c60d72b3d6
Jerry-kjk <jerrykajik@gmail.com>
2023-09-29 07:23:12 -0700

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,6 @@ Fetches https://alx-intranet.hbtn.io/status.
 
 import requests
 
-
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}"
