14934b046140611dd3fea7e13dd3e69b4d0da760
Jerry-kjk <jerrykajik@gmail.com>
2023-10-24 13:34:29 -0700

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+const fs = require('fs');
+const apiUrl = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(apiUrl, (err, response, body) => {
+  if (err === null) {
+    fs.writeFile(filePath, body, 'utf8', (fileError) => {
+      if (fileError !== null) {
+        console.log(fileError);
+      }
+    });
+  } else {
+    console.log(err);
+  }
+});
